3543dd3e73f8e8927af2deb0753746774f33a161
Cole Dennis <coleedennis@gmail.com>
2022-09-25 12:16:22 -0700

    Initial Commit

---
+++ SwiftChartsTutorial/SwiftChartsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsTutorialApp.swift
+//  SwiftChartsTutorial
+//
+//  Created by Cole Dennis on 9/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
